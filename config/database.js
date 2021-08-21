80de6027c70774988af064501377190b7d2a7a5b
유민호 <wingedcompany@gmail.com>
2021-08-22 00:03:30 +0900

    gitignore

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const initOptions = {
-    query(e) {
-        console.log('QUERY:', e.query);
-    }
-};
-const pgp = require("pg-promise")(initOptions);
-const db = pgp("postgres://postgres:minho1010@localhost:5432/pg_db");
-module.exports = db
